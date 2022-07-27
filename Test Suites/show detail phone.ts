fadbac6e51ca34235d10a14750aa883bce0e67e9
Uswatun251219931 <fajaruswatunchasanah@gmail.com>
2022-07-28 03:41:23 +0700

    commit1

---
+++ Test Suites/show detail phone.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>show detail phone</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>be548722-9bef-4435-9bbb-f2b64204812d</testSuiteGuid>
+</TestSuiteEntity>
